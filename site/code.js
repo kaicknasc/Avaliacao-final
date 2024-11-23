3c597ade4ffd5d5737af82b83469a51ccb20f23f
Kaick <kaicknasc1203@gmail.com>
2024-11-22 21:07:17 -0300

    2107

--- site/code.js
+++ site/code.js
@@ -3,3 +3,43 @@ ScrollReveal().reveal("#buttonS1", {
     origin: 'left',
     distance: '50px'
 })
+
+ScrollReveal().reveal(".image-s1", {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '60px'
+})
+
+ScrollReveal().reveal(".image-s4", {
+    duration: 4000,
+    origin: 'top',
+    distance: '100px'
+});
+
+// Carrosel
+
+$(document).ready(function() {
+
+    // Seleciona o contêiner .carousel e aplica a função slick(). Que ativa o carrosel com as configurações abaixo.
+
+    $(".carrosel").slick({
+
+        // slidesToShow: número de cards exibidos ao mesmo tempo no carrosel.
+
+        slidesToShow: 3,
+
+        // slidesToScroll: define quantos cards serão deslizados ao mover o carrosel.
+
+        slidesToScroll: 2,
+
+        // Infinite: quando true, o carrosel retorna ao início automaticamente.
+
+        infinite: true,
+
+        // dots: quando true, ativa os pontos de navegação abaixo do carrosel
+
+        dots: true,
+
+        arrows: false,
+    });
+});
